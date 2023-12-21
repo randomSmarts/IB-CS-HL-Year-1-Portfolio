08adbc5ee067414844974f783214ecb5fee40229
randomSmarts <aarshsmittal@gmail.com>
2023-12-21 03:05:16 -0800

    Finished coding the QueuePalindrome code
    I AM DONE WITH MY IB CS HL YEAR 1 SEMESTER 1 PORTFOLIO

--- src/QueuePalindromes/PalinList.java
+++ src/QueuePalindromes/PalinList.java
@@ -1,4 +1,36 @@
 package QueuePalindromes;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.LinkedList;
 
 public class PalinList {
-}
+    private Queue<String> queue;
+    private Stack<String> stack;
+    private String myList;
+
+    public PalinList (String list) {
+        myList = list;
+        stack = new Stack<String>();
+        queue = new LinkedList<String>();
+
+        String[] split = myList.split(" ");
+        for (int i = 0; i < split.length; i++) {
+            queue.add(split[i]);
+            stack.push(split[i]);
+        }
+    }
+
+        public boolean isPalin() {
+            for (int i = 0; i < stack .size(); i++) {
+                if (!stack.pop().equals(queue.remove())) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public String toString() {
+            return myList;
+        }
+    }
+
--- src/QueuePalindromes/QueuePalindromeMain.java
+++ src/QueuePalindromes/QueuePalindromeMain.java
@@ -1,4 +1,20 @@
 package QueuePalindromes;
 
 public class QueuePalindromeMain {
+    public static void main (String[] args) {
+        PalinList pl =  new PalinList ("one two three two one");
+        System.out.println(pl + " is " + pl.isPalin());
+
+        PalinList pl2 = new PalinList("one two three three two one");
+        System.out.println(pl2 + " is " + pl2.isPalin());
+
+        PalinList pl3 = new PalinList("one two three two one");
+        System.out.println(pl3 + " is " + pl3.isPalin());
+
+        PalinList pl4 = new PalinList("one two three four five");
+        System.out.println(pl4 + " is " + pl4.isPalin());
+
+        PalinList pl5 = new PalinList("one two three tuo one");
+        System.out.println(pl5 + " is " + pl5.isPalin());
+    }
 }
